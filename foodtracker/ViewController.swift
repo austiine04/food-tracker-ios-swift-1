a318da0ee883e341c827611410af7964b0beaa04
austiine04 <austiine04@gmail.com>
2016-05-29 09:46:18 +1000

    responding to button clicks

--- foodtracker/ViewController.swift
+++ foodtracker/ViewController.swift
@@ -1,5 +1,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    // MARK: Properties
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealNameText: UITextField!
+
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
