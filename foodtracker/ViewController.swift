d38a9cb0ebe5d8b97e0f67017a22f6e1307c7ecc
austiine04 <austiine04@gmail.com>
2016-05-29 10:13:02 +1000

    respond to user pressing done

--- foodtracker/ViewController.swift
+++ foodtracker/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Properties
     @IBOutlet weak var mealNameLabel: UILabel!
@@ -9,6 +9,21 @@ class ViewController: UIViewController {
     // MARK: Actions
     @IBAction func setDefaultLabelText(sender: UIButton) {
         mealNameLabel.text = "Default Text"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mealNameText.delegate = self
+    }
+
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
 }
 
