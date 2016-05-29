cb4c1e193d105c4b304c8d0255f66048521e602e
austiine04 <austiine04@gmail.com>
2016-05-29 12:54:13 +1000

    rating controller with red buttons

--- foodtracker/RatingControl.swift
+++ foodtracker/RatingControl.swift
@@ -1,13 +1,50 @@
-//
-//  RatingControl.swift
-//  foodtracker
-//
-//  Created by Augustine Kisitu on 29/05/2016.
-//  Copyright © 2016 Augustine Kisitu. All rights reserved.
-//
-
-import Cocoa
+import UIKit
 
 class RatingControl: UIView {
 
+    //MARK: Properties
+    var rating: Int = 0
+    var ratingButtons = [UIButton]()
+    var starCount = 5
+    let buttonSize = 44
+
+    //MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        createButtons()
+    }
+
+    //MARK: Overrides
+    override func layoutSubviews() {
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(startingPointOnXAxis(index))
+            button.frame = buttonFrame
+        }
+    }
+
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: buttonSize)
+    }
+
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+
+    //MARK: Private functions
+    private func startingPointOnXAxis(index: Int) -> Int {
+        let spacingBetweenButtons = 5
+        return index * (buttonSize + spacingBetweenButtons)
+    }
+
+    private func createButtons() {
+        for _ in 0..<starCount {
+            let button = UIButton()
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            ratingButtons.append(button)
+            addSubview(button)
+        }
+    }
 }
