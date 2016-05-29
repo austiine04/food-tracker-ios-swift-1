910ef4dd84156cf25d5b01265ec596e7636f5068
austiine04 <austiine04@gmail.com>
2016-05-29 11:42:23 +1000

    add custom view to stack view and fixes auto layout warnings

---
+++ foodtracker/RatingControl.swift
@@ -0,0 +1,13 @@
+//
+//  RatingControl.swift
+//  foodtracker
+//
+//  Created by Augustine Kisitu on 29/05/2016.
+//  Copyright © 2016 Augustine Kisitu. All rights reserved.
+//
+
+import Cocoa
+
+class RatingControl: UIView {
+
+}
