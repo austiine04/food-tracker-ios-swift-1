67edc776de9587ddec6a9172a37511d8465aaf54
austiine04 <austiine04@gmail.com>
2016-05-29 11:17:33 +1000

    make image view respond to user touch

--- foodtracker/ViewController.swift
+++ foodtracker/ViewController.swift
@@ -1,16 +1,26 @@
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     // MARK: Properties
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var mealNameText: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
 
     // MARK: Actions
     @IBAction func setDefaultLabelText(sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
 
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        mealNameText.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .PhotoLibrary
+        imagePickerController.delegate = self
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         mealNameText.delegate = self
@@ -24,6 +34,17 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     func textFieldDidEndEditing(textField: UITextField) {
         mealNameLabel.text = textField.text
+    }
+
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageView.image = selectedImage
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
 
