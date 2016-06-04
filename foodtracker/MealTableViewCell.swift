de45c9596fe4262e9b4432032645e4f46f74f530
austiine04 <austiine04@gmail.com>
2016-06-04 14:21:03 +1000

    add a table view controller to display list of meals

--- foodtracker/MealTableViewCell.swift
+++ foodtracker/MealTableViewCell.swift
@@ -2,4 +2,16 @@ import UIKit
 
 class MealTableViewCell: UITableViewCell {
 
+    //MARK: properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
 }
---
+++ foodtracker/MealTableViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+class MealTableViewController: UITableViewController {
+    var meals = [Meal]()
+
+    func loadSampleMeals() {
+        let photo1 = UIImage(named: "meal1")!
+        meals.append(Meal(name: "Caprese Salad", photo: photo1, rating: 4)!)
+        
+        let photo2 = UIImage(named: "meal2")!
+        meals.append(Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5)!)
+        
+        let photo3 = UIImage(named: "meal3")!
+        meals.append(Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3)!)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadSampleMeals()
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "MealTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MealTableViewCell
+        let meal = meals[indexPath.row]
+        
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+        
+        return cell
+    }
+}
