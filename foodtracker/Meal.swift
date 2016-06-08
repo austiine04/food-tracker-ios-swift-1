9338d14d9f4704cdc3fad8770a338113f2842d38
austiine04 <austiine04@gmail.com>
2016-06-08 22:54:31 +1000

    prepares model for persistance

--- foodtracker/Meal.swift
+++ foodtracker/Meal.swift
@@ -1,8 +1,17 @@
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
     
     // MARK: Properties
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+
     var name: String
     var photo: UIImage?
     var rating: Int
@@ -12,9 +21,25 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+        super.init()
         
         if name.isEmpty || rating < 0 {
             return nil
         }
+    }
+
+    //MARK: NSCoding
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+
+    //MARK: convienience initializer
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.photoKey)
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
